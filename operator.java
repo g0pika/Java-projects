c21d5359f0fc234de8d28a10c403402070d878ff
Gopika G <71868599+g0pika@users.noreply.github.com>
2023-01-30 13:52:53 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+        int result = x + y;
+        System.out.println("Result: " + result);
+
+    }
+}
